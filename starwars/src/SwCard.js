cf620186a0af6f360ed1024117a4350ea8d5588c
Leisha <banksleisha@yahoo.com>
2019-11-22 23:34:19 -0500

    MVP

--- starwars/src/SwCard.js
+++ starwars/src/SwCard.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
 
-const Grid = styled.div`
-display: grid;`
 
 const Container = styled.div`
 background: white;
 opacity: 0.6;
+margin: 0 auto;
 width: 40%;
-height: 60%;`
+height: 200px;`
 
 const Title = styled.div`
+font-size: 25px;
+color: #CC5500;`
+
+const Gender = styled.p`
 font-size: 20px;
-color: orange;`
+color: #A64500;`
+
+const Birth = styled.p`
+font-size: 20px;
+color: #803500;`
+
+const Weight = styled.p`
+font-size: 20px;
+color: #592500;`
   
         
  const SwCard = (props) => {
@@ -21,9 +32,9 @@ color: orange;`
         <Container className="container">
          <Title key={props.id}>
             <h3> {props.name}</h3>
-            <p>Gender: {props.gender}</p>
-            <p>Birth Year: {props.birth_year}</p>
-            <p>Weight: {props.weight}</p>
+            <Gender>Gender: {props.gender}</Gender>
+            <Birth>Birth Year: {props.birth_year}</Birth>
+            <Weight>Weight: {props.weight}lbs</Weight>
         </Title>
         </Container>
       </div>  
